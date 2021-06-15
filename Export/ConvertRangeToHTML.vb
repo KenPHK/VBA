4df3cae3568ec77fc731feaaab82749df1410f55
KenPHK <35717587+KenPHK@users.noreply.github.com>
2021-06-15 16:34:42 +0800

    Create ConvertRangeToHTML.vb

---
+++ Export/ConvertRangeToHTML.vb
@@ -0,0 +1,38 @@
+Sub ConvertRangeToHTML( _
+		ByVal path As String, _
+		ByVal rng As Range, _
+		Optional VisibleOnly As Boolean = False)
+	
+	' Remove hidden cell
+	If VisibleOnly Then
+		rng.SpecialCells(xlTypeVisible).Copy
+		
+		Dim wkb As Workbook
+		Set wkb = Workbook.Add
+		
+		With wkb.Sheets(1).Cells(1)
+			.PasteSpecial Paste:=8
+			.PasteSpecial xlPasteValues, , False, False
+			.PasteSpecial xlPasteFormats, , False, False
+			.Select
+		End With
+		
+		Application.CutCopyMode = False
+		
+		Set rng = wkb.Sheets(1).Usedrange
+	End If
+	
+	' Publish range in HTML format
+	ActiveWorkbook.PublishObjects.Add( _
+		SourceType:=xlSourceRange, _
+		Filename:=path, _
+		Sheet:= rng.Worksheet.Name, _
+		Source:=rng.Address, _
+		HTMLType:=xlHTMLStatic).Publish
+	
+	' Close temporary workbook if open
+	On Error Resume Next
+		Call wkb.Close(False, False)
+	On Error Goto 0
+End Sub
+	
